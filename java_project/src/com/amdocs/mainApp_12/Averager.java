dcf9be1c78b81d4037f376e314d727813e169bac
Amit Patil (Krishna) <gpamitranjit@gmail.com>
2018-01-18 12:20:32 +0530

    updating the Averager class

--- java_project/src/com/amdocs/mainApp_12/Averager.java
+++ java_project/src/com/amdocs/mainApp_12/Averager.java
@@ -1,19 +1,24 @@
 package com.amdocs.mainApp_12;
 
-import java.util.function.BiConsumer;
+public class Averager{
 
-public class Averager implements BiConsumer<Averager,Double> {
+	private double total = 0;
+	private double count = 0;
 
-	private int total = 0;
-	private int count = 0;
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public double getCount() {
+		return count;
+	}
+
+	public void setCount(double count) {
+		this.count = count;
+	}
 	
-	public void combine(Averager avg1, Averager avg2) {
-		
-	}
-
-	@Override
-	public void accept(Averager arg0, Double arg1) {
-		// TODO Auto-generated method stub
-		
-	}
 }
