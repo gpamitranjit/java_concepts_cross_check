2db7c113ee8d3c710eb4cb009ca04032e057f02e
Amit Patil (Krishna) <gpamitranjit@gmail.com>
2018-01-22 10:56:01 +0530

    collect stream of data into a map using 2nd version

---
+++ java_project/src/com/amdocs/mainApp_12/MainApp_12_42.java
@@ -0,0 +1,18 @@
+package com.amdocs.mainApp_12;
+
+/**
+ * The third version listed as follows allows us to use supplier to provide a map object
+ * 
+ * toMap(Function<? super T,? extends K> keyMapper, Function<? super T,? extends U> valueMapper, BinaryOperator<U> mergeFunction, Supplier<M>  mapSupplier)
+ * 
+ * @author amit
+ *
+ */
+public class MainApp_12_42 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
