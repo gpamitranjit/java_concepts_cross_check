80e3bc7a94b1c686861db04b0fc2bd863d474aa6
Amit Patil (Krishna) <gpamitranjit@gmail.com>
2018-01-22 10:52:14 +0530

    collect stream of data into a map using 2nd version

---
+++ java_project/src/com/amdocs/mainApp_12/MainApp_12_40.java
@@ -0,0 +1,45 @@
+package com.amdocs.mainApp_12;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.amdocs.mainApp_12.Employee.Gender;
+
+/**
+ * The second form of toMap() listed as follows has one extra merge function
+ * 
+ * toMap(Function<? super T,? extends K> keyMapper, Function<? super T,? extends U> valueMapper, BinaryOperator<U> mergeFunction)
+ * 
+ * 
+ * The merged function is passed the old and new values for the duplicate key. the function should merge the two values and return a new value that will be used for the key
+ * 
+ * @author amit
+ *
+ */
+public class MainApp_12_40 {
+
+	public static void main(String[] args) {
+		List<Employee> persons = MainApp_12_40.persons();
+		Map<Employee.Gender, String> genderToNamesMap = persons
+			.stream()
+			.collect(Collectors.toMap(Employee::getGender, Employee::getName, (oldValue, newValue)-> String.join(", ", oldValue, newValue)));
+		System.out.println(genderToNamesMap);
+	}
+
+	public static List<Employee> persons() {
+	    Employee p1 = new Employee(1, "Jake", Gender.MALE, LocalDate.of(1971,Month.JANUARY, 1), 2000.0);
+	    Employee p2 = new Employee(2, "Jack", Gender.MALE, LocalDate.of(1972,Month.JULY, 21), 3000.0);
+	    Employee p3 = new Employee(3, "Jane", Gender.FEMALE, LocalDate.of(1973,Month.MAY, 29), 4000.0);
+	    Employee p4 = new Employee(4, "Jode", Gender.MALE, LocalDate.of(1974,Month.OCTOBER, 16), 7000.0);
+	    Employee p5 = new Employee(5, "Jake", Gender.FEMALE, LocalDate.of(1975,Month.DECEMBER, 13), 8000.0);
+	    Employee p6 = new Employee(6, "Jake", Gender.MALE, LocalDate.of(1976,Month.JUNE, 9), 6000.0);
+
+	    List<Employee> persons = Arrays.asList(p1, p2, p3, p4, p5, p6);
+
+	    return persons;
+	}
+}
